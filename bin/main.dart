a67e1c5f9dd18af4552e5bf94aa42491997c139d
sntsb <sntsb.doc@gmail.com>
2025-11-18 07:27:01 -0300

    refactor: restructure application entry point and remove unused code

---
+++ bin/main.dart
@@ -0,0 +1,7 @@
+import 'package:asynchronism_app/asynchronism_app.dart' as asynchronism_app;
+
+void main() {
+  print('Hello world!');
+
+  asynchronism_app.runAsynchronismApp();
+}
--- lib/asynchronism_app.dart
+++ lib/asynchronism_app.dart
@@ -1,3 +1,20 @@
-int calculate() {
-  return 6 * 7;
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart' show Response;
+
+void runAsynchronismApp() {
+  print('Asynchronism App Started');
+
+  requestData();
+}
+
+void requestData() {
+  String url =
+      "https://gist.githubusercontent.com/sntsbia/7a295e5b169be3438ed587cc1a86c8f8/raw/e7f5ae15b56afe64be1af9b8253baec3ddc36ef5/accounts.json";
+
+  Future<Response> futureResponse = http.get(Uri.parse(url));
+
+  futureResponse.then((Response response) {
+    print("Status code: ${response.statusCode}");
+    print(response.body);
+  });
 }
